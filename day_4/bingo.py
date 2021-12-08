9fdc2cd0dabb58de570a0947427802ce937675c2
Daniel Flores Diaz <daniel.flores@Daniels-MacBook-Pro.local>
2021-12-07 19:04:01 -0600

    Part One to know the first bingo card to win

--- day_4/bingo.py
+++ day_4/bingo.py
@@ -23,6 +23,7 @@ def obtain_input():
     return first_line, boards
 
 def winner_board():
+    total = 0
     bingo_numbers, boards = obtain_input()
     counts = np.zeros(shape=(len(boards), 5, 5))
     for number in bingo_numbers:
@@ -34,9 +35,23 @@ def winner_board():
 
         for j, count in enumerate(counts):
             if any(n for n in count.sum(axis=0) == 5) or any(j for j in count.sum(axis=1) == 5):
+                marked_board = count
+                board_number = j
+                winner_number = number
+                for i, board in enumerate(boards):
+                    if i == board_number:
+                        normal_winner_board = board
                 break
         else:
             continue
         break
-    print(counts)
+    
+    #* Print the number of the winner board to know the list of the numbers
+    for x in range(5):
+        for y in range(5):
+            if int(marked_board[x][y]) == 0:
+                total += int((normal_winner_board[x][y]))
+    print(f"Last Number: {winner_number}, \nBoard: {board_number},\nMarked Numbers:\n{marked_board},\nScore: {total*winner_number}")
+            
+
 winner_board()
